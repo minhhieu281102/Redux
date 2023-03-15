ecc2c5adb539f1bff659d15384847a2a7abe3c23
hieunguyen <hieunb20@gmail.com>
2023-03-15 16:58:32 +0700

    add

---
+++ local-blog/src/pages/blog/blog.slice.ts
@@ -0,0 +1,93 @@
+import {
+  createReducer,
+  createAction,
+  current,
+  PayloadAction,
+  nanoid,
+  createSlice,
+  createAsyncThunk
+} from '@reduxjs/toolkit'
+import { initalPostList } from 'constants/blog'
+import { Post } from 'types/blog.type'
+import http from 'utils/http'
+
+interface BlogState {
+  postList: Post[]
+  editingPost: Post | null
+}
+
+const initialState: BlogState = {
+  postList: [],
+  editingPost: null
+}
+
+export const getPostList = createAsyncThunk('blog/getPostList', async (_, thunkAPI) => {
+  const response = await http.get<Post[]>('posts', {
+    signal: thunkAPI.signal
+  })
+  return response.data
+})
+const blogSlice = createSlice({
+  name: 'blog',
+  initialState,
+  reducers: {
+    deletePost: (state, action: PayloadAction<string>) => {
+      const postId = action.payload
+      const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
+      if (foundPostIndex !== -1) {
+        state.postList.splice(foundPostIndex, 1)
+      }
+    },
+    startEditingPost: (state, action: PayloadAction<string>) => {
+      const postId = action.payload
+      const foundPost = state.postList.find((post) => post.id === postId) || null
+      state.editingPost = foundPost
+    },
+    cancelEditingPost: (state) => {
+      state.editingPost = null
+    },
+    finishEditingPost: (state, action: PayloadAction<Post>) => {
+      const postId = action.payload.id
+      state.postList.some((post, index) => {
+        if (post.id === postId) {
+          state.postList[index] = action.payload
+          return true
+        }
+        return false
+      })
+      state.editingPost = null
+    },
+    addPost: {
+      reducer: (state, action: PayloadAction<Post>) => {
+        const post = action.payload
+        state.postList.push(post)
+      },
+      prepare: (post: Omit<Post, 'id'>) => ({
+        payload: {
+          ...post,
+          id: nanoid()
+        }
+      })
+    }
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(getPostList.fulfilled, (state, action) => {
+        state.postList = action.payload
+      })
+      .addMatcher(
+        (action) => action.type.includes('cancel'),
+        (state, action) => {
+          console.log(current(state))
+        }
+      )
+      .addDefaultCase((state, action) => {
+        console.log(`action type: ${action.type}`, current(state))
+      })
+  }
+})
+
+export const { addPost, cancelEditingPost, deletePost, finishEditingPost, startEditingPost } = blogSlice.actions
+const blogReducer = blogSlice.reducer
+
+export default blogReducer
